772bef6819562950968e49d3e0ed917d67c92895
Mateus Nogueira <mnogueira030@gmail.com>
2026-04-07 21:21:37 -0300

    Ajustes textos, ajuste traduções, redução de código

--- Functions.js
+++ Functions.js
@@ -2,28 +2,34 @@ const translations = {
     en: {
         "welcome.title": "Welcome to Our Website",
         "welcome.message": "We are glad you are here!",
-        "index.aboutme": "I started working in IT in May 2018, as a programmer and IT Infrastructure Support, helping with the maintenance and installation of the computers at the company where I worked during that period." +
-                        "\n Primarily using Delphi with SQL Server and Firebird databases." + 
-                        "\n\n Sendo esse o foco durante quase 3 anos. Por volta de 3 meses durante este período também atuei como desenvolvedor mobile, utilizando principalmente Kotlin, Android Studio e Node Js." + 
-                        "\n\n Depois disso, por mais de 4 anos, trabalhei como consultor B.I. , utilizando principalmente as tecnologias Qlik Sense, Qlik View e bancos de dados diversos." + 
-                        "\n\n Atualmente meu principal objetivo é voltar a trabalhar com programação. É a área específica em que eu mais me destaco e mais tenho vontade de crescer." +
-                        "\n Também é a área que mais me da satisfação de atuar e aprender novas tecnologias." + 
-                        "\n No momento não estou buscando uma linguágem em específico, mas me inserir novamente em um cargo de programador/desenvolvedor." + 
-                        "\n\n Tenho facilidade em aprender novas tecnologias, além de me adaptar rapidamente em ambientes novos." + 
-                        "\n\n\n\n Para contato e mais informações deixo abaixo meus perfis do LinkedIn e GitHub."
+        "index.aboutme": "IT professional with experience since 2018, working in various roles such as programmer, customer support, and, more recently, Business Intelligence consulting." +
+                        "\n\n I'm a calm person, with an aptitude for learning new technologies and adapting to different contexts and work environments." + 
+                        "\n\n Currently, I'm refocusing on software development, with an interest in different areas of programming, including web, mobile, and game development, and also seeking to improve my skills in modern frameworks." +
+                        "\n\n\n Languages: Delphi, Javascript, C#, Java, Python, PHP" +
+                        "\n\n Web: HTML, CSS" +
+                        "\n\n Databases: Firebird, SQLServer, MySQL" +
+                        "\n\n Business Intelligence: Qlik Sense, QlikView, Qlik Cloud, NPrinting, ETL" +
+                        "\n\n Other: Git, Node Js, Unity, Unreal Engine, Microsoft Office" + 
+                        "\n\n\n\n Below are some ways to contact me!",
+        "index.abouttxt": "About",
+        "index.experiencetxt": "Work Experience",
+        "index.updatestxt": "Updates"
     },
-    pt: {
+    ptbr: {
         "welcome.title": "Bem-vindo ao nosso site",
         "welcome.message": "Estamos felizes que você está aqui!",
-        "index.aboutme": "Comecei a trabalhar na área de T.I. em Maio de 2018 como programador e também de suporte técnico, na parte de manutenção e instalação dos computadores da empresa em que eu trabalhava nesse período." +
-                        "\n Utilizando principalmente Delphi e os bancos de dados SQL Server e Firebird." + 
-                        "\n\n Sendo esse o foco durante quase 3 anos. Por volta de 3 meses durante este período também atuei como desenvolvedor mobile, utilizando principalmente Kotlin, Android Studio e Node Js." + 
-                        "\n\n Depois disso, por mais de 4 anos, trabalhei como consultor B.I. , utilizando principalmente as tecnologias Qlik Sense, Qlik View e bancos de dados diversos." + 
-                        "\n\n Atualmente meu principal objetivo é voltar a trabalhar com programação. É a área específica em que eu mais me destaco e mais tenho vontade de crescer." +
-                        "\n Também é a área que mais me da satisfação de atuar e aprender novas tecnologias." + 
-                        "\n No momento não estou buscando uma linguágem em específico, mas me inserir novamente em um cargo de programador/desenvolvedor." + 
-                        "\n\n Tenho facilidade em aprender novas tecnologias, além de me adaptar rapidamente em ambientes novos." + 
-                        "\n\n\n\n Para contato e mais informações deixo abaixo meus perfis do LinkedIn e GitHub."
+        "index.aboutme": "Profissional de TI com experiência desde 2018, atuando em diferentes funções como programador, suporte ao cliente e, mais recentemente, consultoria em Business Intelligence." +
+                        "\n\n Sou uma pessoa calma, com facilidade para aprender novas tecnologias e me adaptar a diferentes contextos e ambientes de trabalho." + 
+                        "\n\n Atualmente estou focando novamente em desenvolvimento de software, com interesse em diferentes áreas da programação, incluindo desenvolvimento Web, Mobile e jogos, além de estar buscando evoluir em frameworks modernos." + 
+                        "\n\n\n Linguagens: Delphi, Javascript, C#, Java, Python, PHP" +
+                        "\n\n Web: HTML, CSS" +
+                        "\n\n Bancos de dados: Firebird, SQLServer, MySQL" +
+                        "\n\n Business Intelligence: Qlik Sense, QlikView, Qlik Cloud, NPrinting, ETL" +
+                        "\n\n Outros: Git, Node Js, Unity, Unreal Engine, Microsoft Office" + 
+                        "\n\n\n\n Deixo abaixo algumas formas para contato!",
+        "index.abouttxt": "Sobre",
+        "index.experiencetxt": "Experiência",
+        "index.updatestxt": "Updates"
     }
 };
 
@@ -37,7 +43,7 @@ function setLanguage(language) {
     // Optional: save user preference to localStorage
     localStorage.setItem('preferredLang', language);
 
-    if(language == 'pt') {
+    if(language == 'ptbr') {
         const iconBr = document.getElementById("BrazilIcon");
         iconBr.classList.add("BorderLangIcon");
         const iconUs = document.getElementById("UsaIcon");
@@ -53,6 +59,6 @@ function setLanguage(language) {
 
 // Set the default or saved language on page load
 document.addEventListener('DOMContentLoaded', () => {
-    const savedLang = localStorage.getItem('preferredLang') || 'pt';
+    const savedLang = localStorage.getItem('preferredLang') || 'ptbr';
     setLanguage(savedLang);
 });
