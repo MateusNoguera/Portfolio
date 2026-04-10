df99eb2d149ebf75f5297219b15739c29d63f777
Mateus Nogueira <mnogueira030@gmail.com>
2026-04-10 12:05:51 -0300

    Melhoria css experience, inclusão de textos e traduções

--- Functions.js
+++ Functions.js
@@ -11,15 +11,26 @@ const translations = {
                         "\n\n Business Intelligence: Qlik Sense, QlikView, Qlik Cloud, NPrinting, ETL" +
                         "\n\n Other: Git, Node Js, Unity, Unreal Engine, Microsoft Office" + 
                         "\n\n\n\n Contact information is provided below.",
-        "index.abouttxt": "About",
-        "index.experiencetxt": "Work Experience",
-        "index.updatestxt": "Updates",
+        "site.abouttxt": "About",
+        "site.experiencetxt": "Work Experience",
+        "site.updatestxt": "Updates",
         "experience.txtbilden": "Bilden Resultados com Inteligência" +
                                 "\n June 2021 - August 2025",
         "experience.infobilden": "Developing and Managing Business Intelligence projects primarily using Qlik Cloud, Qlik Sense, Qlik View, NPrinting.",
         "experience.txtgeovendas": "GEOvendas" +
                                 "\n February 2021 - March 2021",
-        "experience.infogeovendas": "Support, assisting customers in using and resolving problems on the company systems, as well as performing tests and identifying problems."
+        "experience.infogeovendas": "Support, assisting customers in using and resolving problems on the company systems, as well as performing tests and identifying problems.",
+        "experience.txtbytecom": "Bytecom Sistemas" +
+                                "\n September 2020 - November 2020",
+        "experience.infobytecom": "Delphi developer, front-end and back-end, implementing and managing features in the company's Enterprise Resource Planning system." +
+                                "\n Mobile Developer, using Kotlin, Android Studio and Node Js.",
+        "experience.txtdessis": "Dessis Sistemas" +
+                                "\n May 2018 - August 2020",
+        "experience.infodessis": "Delphi developer, front-end and back-end, implementing and managing functionalities in the company's systems, along with the Firebird and SQL Server databases.",
+        "experience.languagestxt": "Languages",
+        "experience.languagetxtptbr": "Portuguese - Native",
+        "experience.languagetxtusa": "English - Advanced/Fluent",
+        "experience.languagetxtjapan": "Japanese - Beginner"
     },
     ptbr: {
         "index.completename": "Mateus Nogueira",
@@ -33,15 +44,26 @@ const translations = {
                         "\n\n Business Intelligence: Qlik Sense, QlikView, Qlik Cloud, NPrinting, ETL" +
                         "\n\n Outros: Git, Node Js, Unity, Unreal Engine, Microsoft Office" + 
                         "\n\n\n\n Segue abaixo algumas formas para contato.",
-        "index.abouttxt": "Sobre",
-        "index.experiencetxt": "Experiência",
-        "index.updatestxt": "Updates",
+        "site.abouttxt": "Sobre",
+        "site.experiencetxt": "Experiência",
+        "site.updatestxt": "Updates",
         "experience.txtbilden": "Bilden Resultados com Inteligência" +
                                 "\n Junho 2021 - Agosto 2025",
         "experience.infobilden": "Desenvolvendo e fazendo manutenção de projetos de Business Intelligence utilizando principalmente Qlik Sense, Qlik View, Qlik Cloud, Nprinting.",
         "experience.txtgeovendas": "GEOvendas" +
                                 "\n Fevereiro 2021 - Março 2021",
-        "experience.infogeovendas": "Suporte, auxiliando clientes a resolver problemas e navegar nos sistemas, além de fazer testes e identificar problemas."
+        "experience.infogeovendas": "Suporte, auxiliando clientes a resolver problemas e navegar nos sistemas, além de fazer testes e identificar problemas.",
+        "experience.txtbytecom": "Bytecom Sistemas" +
+                                "\n Setembro 2020 - Novembro 2020",
+        "experience.infobytecom": "Desenvolvedor Delphi, front-end e back-end, implementando e gerenciando funcionalidades no sistema ERP da empresa." +
+                                "\n Desenvolvedor Mobile, usando Kotlin, Android Studio e Node Js.",
+        "experience.txtdessis": "Dessis Sistemas" +
+                                "\n Maio 2018 - Agosto 2020",
+        "experience.infodessis": "Desenvolvedor Delphi, front-end e back-end, implementando e gerenciando funcionalidades nos sistemas da empresa, juntamente com os bancos de dados Firebird e SQLServer.",
+        "experience.languagestxt": "Idiomas",
+        "experience.languagetxtptbr": "Português - Nativo",
+        "experience.languagetxtusa": "Inglês - Avançado/Fluente",
+        "experience.languagetxtjapan": "Japonês - Iniciante"
     }
 };
 
