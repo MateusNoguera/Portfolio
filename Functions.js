2e66f36585cf07f3be8dd8568a7d820bea8ebbb2
Mateus Nogueira <mnogueira030@gmail.com>
2026-05-06 14:13:03 -0300

    Reestruturação de código.

--- Functions.js
+++ Functions.js
@@ -1,104 +1,33 @@
 const translations = {
     en: {
-        "index.completename": "Mateus Nogueira",
-        "footer.devname": "by Mateus Nogueira",
-        "index.aboutme": "IT professional with experience since 2018, working in various roles such as programmer, customer support, and, more recently, Business Intelligence consulting." +
-                        "\n\n I'm a calm person, with an aptitude for learning new technologies and adapting to different contexts and work environments." + 
-                        "\n\n Currently, I'm refocusing on software development, with an interest in different areas of programming, including web, mobile, and game development, and also seeking to improve my skills in new tools and languages." +
-                        "\n\n\n Recently used: " +
-                        "\n\n HTML, CSS, Javascript, Git, Qlik Sense, QlikView, Qlik Cloud, Nprinting." +
-                        "\n\n\n\n Others: " +
-                        "\n\n Delphi, Node Js, Firebird, SQLServer, MySQL, C#, Java, Unity, Unreal Engine, Microsoft Office, Python, PHP" + 
-                        "\n\n\n\n Contact information is provided below.",
-        "site.abouttxt": "About",
-        "site.experiencetxt": "Work Experience",
-        "site.roadmaptxt": "Roadmap",
-        "site.phone": "+55 47 9 9246-2691",
-        "experience.txtbilden": "Bilden Resultados com Inteligência" +
-                                "\n June 2021 - August 2025",
-        "experience.infobilden": "Developing and Managing Business Intelligence projects primarily using Qlik Cloud, Qlik Sense, Qlik View, NPrinting.",
-        "experience.txtgeovendas": "GEOvendas" +
-                                "\n February 2021 - March 2021",
-        "experience.infogeovendas": "Support, assisting customers in using and resolving problems on the company systems, as well as performing tests and identifying problems.",
-        "experience.txtbytecom": "Bytecom Sistemas" +
-                                "\n September 2020 - November 2020",
-        "experience.infobytecom": "Delphi developer, front-end and back-end, implementing and managing features in the company's Enterprise Resource Planning system." +
-                                "\n Mobile Developer, using Kotlin, Android Studio and Node Js.",
-        "experience.txtdessis": "Dessis Sistemas" +
-                                "\n May 2018 - August 2020",
-        "experience.infodessis": "Delphi developer, front-end and back-end, implementing and managing functionalities in the company's systems, along with the Firebird and SQL Server databases.",
-        "experience.languagestxt": "Languages",
-        "experience.languagetxtptbr": "Portuguese - Native",
-        "experience.languagetxtusa": "English - Advanced/Fluent",
-        "experience.languagetxtjapan": "Japanese - Beginner",
-        "experience.educationtxt": "Education",
-        "education.txtsenai": "SENAI/SC - Serviço Nacional de Aprendizagem Industrial",
-        "education.bachelortxt": "Bachelor of Technology - BTech, Internet Systems" +
-                                "\n 2016 - 2019.",
-        "education.ittechniciantxt": "IT Technician." +
-                                    "\n 2015 - 2016.",
-        "education.highschooltxt": "High School." +
-                                    "\n 2014 - 2016.",
-        "roadmap.projecttxt": "Projects",
-        "roadmap.infoprojecttxt": "Create a new tab to display and comment on created projects.",
-        "roadmap.responsivenesstxt": "Responsiveness",
-        "roadmap.inforesponsivenesstxt": "Adjust to automatically adapt the display to different devices and resolutions.",
-        "roadmap.uiuxtxt": "UI/UX & Animations",
-        "roadmap.infouiuxtxt": "Improve the website's visuals, animations, and interactions.",
-        "roadmap.completedstatustxt": "COMPLETED",
-        "roadmap.upcomingstatustxt": "UPCOMING",
-        "roadmap.indevelopmentstatustxt": "IN DEVELOPMENT"
-        
+        "header.resumecv": "Resume (CV)",
+        "header.roadmap": "Roadmap",
+        "header.projects": "Projects",
+        "main.dev.name": "Mateus Nogueira",
+        "main.dev.description": 
+                            "Developer with experience in Business Intelligence and software development, interested in web applications, backend, APIs and game development." +
+                            "\n\nI'm a calm person, with an aptitude for learning new technologies and adapting to different contexts and work environments." +
+                            "\n\nRecently used: HTML, CSS, JavaScript, Git, Qlik Sense, QlikView, Qlik Cloud, NPrinting." + 
+                            "\n\nOther technologies: Delphi, Node.js, Firebird, SQL Server, MySQL, C#, Java, Unity, Unreal Engine, Microsoft Office, Python, PHP.",
+        "main.experiencetitle": "Experience",
+        "main.experience.bilden.name": "Bilden Resultados com Inteligência",
+        "main.experience.bilden.period": "June 2021 - August 2025",
+        "experience.bilden.description": "Developing and Managing Business Intelligence projects primarily using Qlik Cloud, Qlik Sense, Qlik View, NPrinting."
     },
     ptbr: {
-        "index.completename": "Mateus Nogueira",
-        "footer.devname": "by Mateus Nogueira",
-        "index.aboutme": "Profissional de TI com experiência desde 2018, atuando em diferentes funções como programador, suporte ao cliente e, mais recentemente, consultoria em Business Intelligence." +
-                        "\n\n Sou uma pessoa calma, com facilidade para aprender novas tecnologias e me adaptar a diferentes contextos e ambientes de trabalho." + 
-                        "\n\n Atualmente estou focando novamente em desenvolvimento de software, com interesse em diferentes áreas da programação, incluindo desenvolvimento web, mobile e jogos, além de estar buscando evoluir em novas ferramentas." + 
-                        "\n\n\n Tecnologias usadas recentemente: " +
-                        "\n\n HTML, CSS, Javascript, Git, Qlik Sense, QlikView, Qlik Cloud, Nprinting." +
-                        "\n\n\n\n Outras tecnologias: " +
-                        "\n\n Delphi, Node Js, Firebird, SQLServer, MySQL, C#, Java, Unity, Unreal Engine, Microsoft Office, Python, PHP" + 
-                        "\n\n\n\n Segue abaixo algumas formas para contato.",
-        "site.abouttxt": "Sobre",
-        "site.experiencetxt": "Experiência",
-        "site.roadmaptxt": "Roadmap",
-        "site.phone": "+55 47 9 9246-2691",
-        "experience.txtbilden": "Bilden Resultados com Inteligência" +
-                                "\n Junho 2021 - Agosto 2025",
-        "experience.infobilden": "Desenvolvendo e fazendo manutenção de projetos de Business Intelligence utilizando principalmente Qlik Sense, Qlik View, Qlik Cloud, Nprinting.",
-        "experience.txtgeovendas": "GEOvendas" +
-                                "\n Fevereiro 2021 - Março 2021",
-        "experience.infogeovendas": "Suporte, auxiliando clientes a resolver problemas e navegar nos sistemas, além de fazer testes e identificar problemas.",
-        "experience.txtbytecom": "Bytecom Sistemas" +
-                                "\n Setembro 2020 - Novembro 2020",
-        "experience.infobytecom": "Desenvolvedor Delphi, front-end e back-end, implementando e gerenciando funcionalidades no sistema ERP da empresa." +
-                                "\n Desenvolvedor Mobile, usando Kotlin, Android Studio e Node Js.",
-        "experience.txtdessis": "Dessis Sistemas" +
-                                "\n Maio 2018 - Agosto 2020",
-        "experience.infodessis": "Desenvolvedor Delphi, front-end e back-end, implementando e gerenciando funcionalidades nos sistemas da empresa, juntamente com os bancos de dados Firebird e SQLServer.",
-        "experience.languagestxt": "Idiomas",
-        "experience.languagetxtptbr": "Português - Nativo",
-        "experience.languagetxtusa": "Inglês - Avançado/Fluente",
-        "experience.languagetxtjapan": "Japonês - Iniciante",
-        "experience.educationtxt": "Formação",
-        "education.txtsenai": "SENAI/SC - Serviço Nacional de Aprendizagem Industrial",
-        "education.bachelortxt": "Curso Superior de Tecnologia (CST), Tecnólogo em Sistemas para Internet. " +
-                                "\n Cursado entre 2016 - 2019.",
-        "education.ittechniciantxt": "Técnico em Informática." +
-                                    "\n Cursado em 2015 - 2016.",
-        "education.highschooltxt": "Ensino médio." +
-                                    "\n Cursado em 2014 - 2016.",
-        "roadmap.projecttxt": "Projetos",
-        "roadmap.infoprojecttxt": "Criar uma nova aba parar mostrar e comentar sobre projetos criados.",
-        "roadmap.responsivenesstxt": "Responsividade",
-        "roadmap.inforesponsivenesstxt": "Ajustar a visualização para adaptar automaticamente em diferentes dispositivos e resoluções.",
-        "roadmap.uiuxtxt": "Interface e Animações",
-        "roadmap.infouiuxtxt": "Melhorar o visual, animações e interações do site.",
-        "roadmap.completedstatustxt": "CONCLUÍDO",
-        "roadmap.upcomingstatustxt": "EM BREVE",
-        "roadmap.indevelopmentstatustxt": "EM DESENVOLVIMENTO"
+        "header.resumecv": "Currículo",
+        "header.roadmap": "Roadmap",
+        "header.projects": "Projetos",
+        "main.dev.name": "Mateus Nogueira",
+        "main.dev.description": 
+                            "Desenvolvedor com experiência em Business Intelligence e desenvolvimento de software, com interesse em aplicações web, backend, API's e desenvolvimento de jogos." +
+                            "\n\nSou uma pessoa calma, com facilidade para aprender novas tecnologias e me adaptar a diferentes contextos e ambientes de trabalho." +
+                            "\n\nTecnologias usadas recentemente: HTML, CSS, JavaScript, Git, Qlik Sense, QlikView, Qlik Cloud, NPrinting." + 
+                            "\n\nOutras tecnologias: Delphi, Node Js, Firebird, SQLServer, MySQL, C#, Java, Unity, Unreal Engine, Microsoft Office, Python, PHP.",
+        "main.experiencetitle": "Experiência",
+        "main.experience.bilden.name": "Bilden Resultados com Inteligência",
+        "main.experience.bilden.period": "Junho 2021 - Agosto 2025",
+        "experience.bilden.description": "Desenvolvendo e fazendo manutenção de projetos de Business Intelligence utilizando principalmente Qlik Sense, Qlik View, Qlik Cloud, Nprinting."
     }
 };
 
@@ -112,20 +41,6 @@ function setLanguage(language) {
     
     localStorage.setItem('preferredLang', language);
 
-    const iconBr = document.getElementById("BrazilIcon");
-    const iconUs = document.getElementById("UsaIcon");
-
-    if(language == 'ptbr') {
-        
-        iconBr.classList.add("BorderLangIcon");
-        iconUs.classList.remove("BorderLangIcon");
-
-    } else if(language == 'en') {
-
-        iconBr.classList.remove("BorderLangIcon");
-        iconUs.classList.add("BorderLangIcon");
-    }
-
 }
 
 //On page load
@@ -133,20 +48,3 @@ document.addEventListener('DOMContentLoaded', () => {
     const savedLang = localStorage.getItem('preferredLang') || 'ptbr';
     setLanguage(savedLang);
 });
-
-function copyPhone() {
-    navigator.clipboard.writeText("+55 47 9 9246-2691");
-
-    const copyPhoneTxt = document.getElementById("CopyPhoneTxt");
-
-    copyPhoneTxt.classList.remove("fa-copy");
-    copyPhoneTxt.classList.add("fa-check");
-    copyPhoneTxt.style.color = "#1fd655";
-
-    setTimeout(() => {
-        copyPhoneTxt.style.color = "";
-        copyPhoneTxt.classList.remove("fa-check");
-        copyPhoneTxt.classList.add("fa-copy");
-    }, 1000);
-
-}
