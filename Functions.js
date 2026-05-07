1808b22388974c61fdb43e1a4cbd6fd31fa9904f
Mateus Nogueira <mnogueira030@gmail.com>
2026-05-07 15:54:03 -0300

    Inclusão de texto, ajuste ícones, inclusão de contato e ícones com animações.

--- Functions.js
+++ Functions.js
@@ -9,10 +9,41 @@ const translations = {
                             "\n\nI'm a calm person, with an aptitude for learning new technologies and adapting to different contexts and work environments." +
                             "\n\nRecently used: HTML, CSS, JavaScript, Git, Qlik Sense, QlikView, Qlik Cloud, NPrinting." + 
                             "\n\nOther technologies: Delphi, Node.js, Firebird, SQL Server, MySQL, C#, Java, Unity, Unreal Engine, Microsoft Office, Python, PHP.",
-        "main.experiencetitle": "Experience",
+        "main.experience.title": "Experience",
         "main.experience.bilden.name": "Bilden Resultados com Inteligência",
         "main.experience.bilden.period": "June 2021 - August 2025",
-        "experience.bilden.description": "Developing and Managing Business Intelligence projects primarily using Qlik Cloud, Qlik Sense, Qlik View, NPrinting."
+        "main.experience.bilden.description": "Developing and Managing Business Intelligence projects primarily using Qlik Sense, QlikView, NPrinting, Qlik Cloud.",
+        "main.experience.geovendas.name": "GEOvendas",
+        "main.experience.geovendas.period": "February 2021 - March 2021",
+        "main.experience.geovendas.description": "Support, assisting customers in using and resolving problems on the company systems, as well as performing tests and identifying problems.",
+        "main.experience.bytecom.name": "Bytecom Sistemas",
+        "main.experience.bytecom.period": "September 2020 - November 2020",
+        "main.experience.bytecom.description": 
+                                                "Delphi developer, front-end and back-end, implementing and managing features in the company's Enterprise Resource Planning system." +
+                                                " Mobile Developer, using Kotlin, Android Studio and Node.Js.",
+        "main.experience.dessis.name": "Dessis Sistemas",
+        "main.experience.dessis.period": "May 2018 - August 2020",
+        "main.experience.dessis.description": "Delphi developer, front-end and back-end, implementing and managing functionalities in the company's systems, along with the Firebird and SQL Server databases.",
+        "main.languages.title": "Languages",
+        "main.languages.portuguese.name": "Portuguese",
+        "main.languages.portuguese.level": "Native",
+        "main.languages.portuguese.description": "I was born and have lived in Brazil my whole life.",
+        "main.languages.english.name": "English",
+        "main.languages.english.level": "Advanced/Fluent",
+        "main.languages.english.description": "I've been learning and using English for over 15 years.",
+        "main.languages.japanese.name": "Japanese",
+        "main.languages.japanese.level": "Beginner",
+        "main.languages.japanese.description": "After listening to japanese music for a few months, I started studying the language as a hobby.",
+        "main.education.title": "Education",
+        "main.education.senai.name": "SENAI/SC - Serviço Nacional de Aprendizagem Industrial",
+        "main.education.bachelor.period": "2016 - 2019.",
+        "main.education.bachelor.description": "Bachelor of Technology - BTech, Internet Systems",
+        "main.education.ittechnician.period": "2015 - 2016",
+        "main.education.ittechnician.description": "IT Technician.",
+        "main.education.highschool.period": "2014 - 2016.",
+        "main.education.highschool.description": "High School.",
+        "main.contact.title": "Contact",
+        "main.contact.dev.phone": "+55 47 9 9246-2691"
     },
     ptbr: {
         "header.resumecv": "Currículo",
@@ -20,14 +51,45 @@ const translations = {
         "header.projects": "Projetos",
         "main.dev.name": "Mateus Nogueira",
         "main.dev.description": 
-                            "Desenvolvedor com experiência em Business Intelligence e desenvolvimento de software, com interesse em aplicações web, backend, API's e desenvolvimento de jogos." +
+                            "Desenvolvedor com experiência em Business Intelligence e desenvolvimento de software, com interesse em aplicações web, backend, APIs e desenvolvimento de jogos." +
                             "\n\nSou uma pessoa calma, com facilidade para aprender novas tecnologias e me adaptar a diferentes contextos e ambientes de trabalho." +
                             "\n\nTecnologias usadas recentemente: HTML, CSS, JavaScript, Git, Qlik Sense, QlikView, Qlik Cloud, NPrinting." + 
-                            "\n\nOutras tecnologias: Delphi, Node Js, Firebird, SQLServer, MySQL, C#, Java, Unity, Unreal Engine, Microsoft Office, Python, PHP.",
-        "main.experiencetitle": "Experiência",
+                            "\n\nOutras tecnologias: Delphi, Node.Js, Firebird, SQL Server, MySQL, C#, Java, Unity, Unreal Engine, Microsoft Office, Python, PHP.",
+        "main.experience.title": "Experiência",
         "main.experience.bilden.name": "Bilden Resultados com Inteligência",
         "main.experience.bilden.period": "Junho 2021 - Agosto 2025",
-        "experience.bilden.description": "Desenvolvendo e fazendo manutenção de projetos de Business Intelligence utilizando principalmente Qlik Sense, Qlik View, Qlik Cloud, Nprinting."
+        "main.experience.bilden.description": "Desenvolvendo e fazendo manutenção de projetos de Business Intelligence utilizando principalmente Qlik Sense, QlikView, Nprinting, Qlik Cloud.",
+        "main.experience.geovendas.name": "GEOvendas",
+        "main.experience.geovendas.period": "Fevereiro 2021 - Março 2021",
+        "main.experience.geovendas.description": "Suporte, auxiliando clientes a resolver problemas e navegar nos sistemas, além de fazer testes e identificar problemas.",
+        "main.experience.bytecom.name": "Bytecom Sistemas",
+        "main.experience.bytecom.period": "Setembro 2020 - Novembro 2020",
+        "main.experience.bytecom.description": 
+                                                "Desenvolvedor Delphi, front-end e back-end, implementando e gerenciando funcionalidades no sistema ERP da empresa." +
+                                                " Desenvolvedor Mobile, usando Kotlin, Android Studio e Node.Js.",
+        "main.experience.dessis.name": "Dessis Sistemas",
+        "main.experience.dessis.period": "Maio 2018 - Agosto 2020",
+        "main.experience.dessis.description": "Desenvolvedor Delphi, front-end e back-end, implementando e gerenciando funcionalidades nos sistemas da empresa, juntamente com os bancos de dados Firebird e SQL Server.",
+        "main.languages.title": "Idiomas",
+        "main.languages.portuguese.name": "Português",
+        "main.languages.portuguese.level": "Nativo",
+        "main.languages.portuguese.description": "Nasci e vivi no Brasil a vida toda.",
+        "main.languages.english.name": "Inglês",
+        "main.languages.english.level": "Avançado/Fluente",
+        "main.languages.english.description": "Estudo e uso inglês há mais de 15 anos.",
+        "main.languages.japanese.name": "Japonês",
+        "main.languages.japanese.level": "Iniciante",
+        "main.languages.japanese.description": "Após ouvir músicas japonesas por alguns meses, comecei a estudar o idioma como hobby.",
+        "main.education.title": "Formação",
+        "main.education.senai.name": "SENAI/SC - Serviço Nacional de Aprendizagem Industrial",
+        "main.education.bachelor.period": "2016 - 2019.",
+        "main.education.bachelor.description": "Curso Superior de Tecnologia (CST), Tecnólogo em Sistemas para Internet.",
+        "main.education.ittechnician.period": "2015 - 2016",
+        "main.education.ittechnician.description": "Técnico em Informática.",
+        "main.education.highschool.period": "2014 - 2016.",
+        "main.education.highschool.description": "Ensino médio.",
+        "main.contact.title": "Contato",
+        "main.contact.dev.phone": "+55 47 9 9246-2691"
     }
 };
 
@@ -48,3 +110,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const savedLang = localStorage.getItem('preferredLang') || 'ptbr';
     setLanguage(savedLang);
 });
+
+function copyPhone() {
+    navigator.clipboard.writeText("+55 47 9 9246-2691");
+}
