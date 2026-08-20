573059a3b0bd2a470ac9b230fd4bcd5f8e2882dd
Mateus Nogueira <mnogueira030@gmail.com>
2026-08-20 15:55:42 -0300

    Refazendo o site com visuais diferentes.

---
+++ js/old_Functions.js
@@ -0,0 +1,232 @@
+const translations = {
+    en: {
+        "site.title": "Mateus Nogueira | Portfolio",
+        "header.home": "Home",
+        "header.experience": "Experience",
+        "header.languages": "Languages",
+        "header.education": "Education",
+        "header.projects": "Projects",
+        "header.contact": "Contact",
+        "main.dev.name": "Mateus Nogueira",
+        "footer.copyright": "© 2026 - All rights reserved",
+        "main.dev.description": 
+                            "Developer with experience in Business Intelligence and software development, interested in programming, primarily backend and game development." +
+                            "\n\nI'm a calm person, with an aptitude for learning new technologies and adapting to different situations." + 
+                            "\n\nExperience since 2018, working in different roles such as programmer, customer support and, more recently, Business Intelligence consulting.",
+        "main.experience.title": "Experience",
+        "main.experience.bilden.name": "Bilden Resultados com Inteligência",
+        "main.experience.bilden.period": "June 2021 - August 2025",
+        "main.experience.bilden.description": "Developing and Managing Business Intelligence projects primarily using Qlik Sense, QlikView, NPrinting, Qlik Cloud.",
+        "main.experience.geovendas.name": "GEOvendas",
+        "main.experience.geovendas.period": "February 2021 - March 2021",
+        "main.experience.geovendas.description": "Support, assisting customers in using and resolving problems on the company systems, as well as performing tests and identifying problems.",
+        "main.experience.bytecom.name": "Bytecom Sistemas",
+        "main.experience.bytecom.period": "September 2020 - November 2020",
+        "main.experience.bytecom.description": 
+                                                "Delphi developer, front-end and back-end, implementing and managing features in the company's Enterprise Resource Planning system." +
+                                                " Mobile Developer, using Kotlin, Android Studio and Node.Js.",
+        "main.experience.dessis.name": "Dessis Sistemas",
+        "main.experience.dessis.period": "May 2018 - August 2020",
+        "main.experience.dessis.description": "Delphi developer, front-end and back-end, implementing and managing functionalities in the company's systems, along with the Firebird and SQL Server databases.",
+        "main.languages.title": "Languages",
+        "main.languages.portuguese.name": "Portuguese",
+        "main.languages.portuguese.level": "Native",
+        "main.languages.portuguese.description": "I was born and have lived in Brazil my whole life.",
+        "main.languages.english.name": "English",
+        "main.languages.english.level": "Advanced/Fluent",
+        "main.languages.english.description": "I've been learning and using English for over 15 years.",
+        "main.languages.japanese.name": "Japanese",
+        "main.languages.japanese.level": "Beginner",
+        "main.languages.japanese.description": "After listening to japanese music for a few months, I started studying the language as a hobby.",
+        "main.education.title": "Education",
+        "main.education.senai.name": "SENAI/SC - Serviço Nacional de Aprendizagem Industrial",
+        "main.education.bachelor.period": "2016 - 2019.",
+        "main.education.bachelor.description": "Bachelor of Technology - BTech, Internet Systems",
+        "main.education.ittechnician.period": "2015 - 2016",
+        "main.education.ittechnician.description": "IT Technician.",
+        "main.education.highschool.period": "2014 - 2016.",
+        "main.education.highschool.description": "High School.",
+        "main.contact.title": "Contact",
+        "main.projects.title": "Projects",
+        "tech.interests.title": "Interested in",
+        "tech.interests.html.title": "HTML",
+        "tech.interests.css.title": "Css",
+        "tech.interests.javascript.title": "JavaScript",
+        "tech.interests.git.title": "Git",
+        "tech.interests.qliksense.title": "Qlik Sense",
+        "tech.interests.nprinting.title": "NPrinting",
+        "tech.interests.qlikcloud.title": "Qlik Cloud",
+        "tech.interests.unity.title": "Unity",
+        "tech.interests.unrealengine.title": "Unreal Engine",
+        "tech.interests.nodejs.title": "Node Js",
+        "tech.interests.csharp.title": "C#",
+        "tech.interests.python.title": "Python"
+    },
+    ptbr: {
+        "site.title": "Mateus Nogueira | Portfólio",
+        "header.home": "Home",
+        "header.experience": "Experiencia",
+        "header.languages": "Idiomas",
+        "header.education": "Formação",
+        "header.projects": "Projetos",
+        "header.contact": "Contato",
+        "main.dev.name": "Mateus Nogueira",
+        "footer.copyright": "© 2026 - Todos os direitos reservados",
+        "main.dev.description": 
+                            "Desenvolvedor com experiência em Business Intelligence e desenvolvimento de software, com interesse em programação, principalmente backend e desenvolvimento de jogos." +
+                            "\n\nSou uma pessoa calma, com facilidade para aprender novas tecnologias e me adaptar a diferentes situações." +
+                            "\n\nExperiência desde 2018, atuando em diferentes funções como programador, suporte ao cliente e, mais recentemente, consultoria em Business Intelligence.",
+        "main.experience.title": "Experiência",
+        "main.experience.bilden.name": "Bilden Resultados com Inteligência",
+        "main.experience.bilden.period": "Junho 2021 - Agosto 2025",
+        "main.experience.bilden.description": "Desenvolvendo e fazendo manutenção de projetos de Business Intelligence utilizando principalmente Qlik Sense, QlikView, Nprinting, Qlik Cloud.",
+        "main.experience.geovendas.name": "GEOvendas",
+        "main.experience.geovendas.period": "Fevereiro 2021 - Março 2021",
+        "main.experience.geovendas.description": "Suporte, auxiliando clientes a resolver problemas e navegar nos sistemas, além de fazer testes e identificar problemas.",
+        "main.experience.bytecom.name": "Bytecom Sistemas",
+        "main.experience.bytecom.period": "Setembro 2020 - Novembro 2020",
+        "main.experience.bytecom.description": 
+                                                "Desenvolvedor Delphi, front-end e back-end, implementando e gerenciando funcionalidades no sistema ERP da empresa." +
+                                                " Desenvolvedor Mobile, usando Kotlin, Android Studio e Node.Js.",
+        "main.experience.dessis.name": "Dessis Sistemas",
+        "main.experience.dessis.period": "Maio 2018 - Agosto 2020",
+        "main.experience.dessis.description": "Desenvolvedor Delphi, front-end e back-end, implementando e gerenciando funcionalidades nos sistemas da empresa, juntamente com os bancos de dados Firebird e SQL Server.",
+        "main.languages.title": "Idiomas",
+        "main.languages.portuguese.name": "Português",
+        "main.languages.portuguese.level": "Nativo",
+        "main.languages.portuguese.description": "Nasci e vivi no Brasil a vida toda.",
+        "main.languages.english.name": "Inglês",
+        "main.languages.english.level": "Avançado/Fluente",
+        "main.languages.english.description": "Estudo e uso inglês há mais de 15 anos.",
+        "main.languages.japanese.name": "Japonês",
+        "main.languages.japanese.level": "Iniciante",
+        "main.languages.japanese.description": "Após ouvir músicas japonesas por alguns meses, comecei a estudar o idioma como hobby.",
+        "main.education.title": "Formação",
+        "main.education.senai.name": "SENAI/SC - Serviço Nacional de Aprendizagem Industrial",
+        "main.education.bachelor.period": "2016 - 2019.",
+        "main.education.bachelor.description": "Curso Superior de Tecnologia (CST), Tecnólogo em Sistemas para Internet.",
+        "main.education.ittechnician.period": "2015 - 2016",
+        "main.education.ittechnician.description": "Técnico em Informática.",
+        "main.education.highschool.period": "2014 - 2016.",
+        "main.education.highschool.description": "Ensino médio.",
+        "main.contact.title": "Contato",
+        "main.projects.title": "Projetos",
+        "tech.interests.title": "Interesses",
+        "tech.interests.html.title": "HTML",
+        "tech.interests.css.title": "Css",
+        "tech.interests.javascript.title": "JavaScript",
+        "tech.interests.git.title": "Git",
+        "tech.interests.qliksense.title": "Qlik Sense",
+        "tech.interests.nprinting.title": "NPrinting",
+        "tech.interests.qlikcloud.title": "Qlik Cloud",
+        "tech.interests.unity.title": "Unity",
+        "tech.interests.unrealengine.title": "Unreal Engine",
+        "tech.interests.nodejs.title": "Node Js",
+        "tech.interests.csharp.title": "C#",
+        "tech.interests.python.title": "Python"
+    }
+};
+
+function setLanguage(language) {
+    document.querySelectorAll('[data-i18n]').forEach(element => {
+        const key = element.getAttribute('data-i18n');
+        if (translations[language] && translations[language][key]) {
+            if (element.tagName === "TITLE") {
+                document.title = translations[language][key];
+            } else {
+                element.textContent = translations[language][key];
+            }
+        }
+    });
+    
+    localStorage.setItem('preferredLang', language);
+
+    document.querySelectorAll('.LanguageButton')
+    .forEach(button => {
+        button.classList.remove('ActiveLanguage');
+    });
+
+    const activeButton = document.querySelector(`[onclick="setLanguage('${language}')"]`);
+
+    if (activeButton) {
+        activeButton.classList.add('ActiveLanguage');
+    }
+
+}
+
+// On page load
+document.addEventListener('DOMContentLoaded', () => {
+    const savedLang = localStorage.getItem('preferredLang') || 'ptbr';
+    setLanguage(savedLang);
+});
+
+window.addEventListener("scroll", () => {
+
+  const header = document.querySelector(".Header");
+
+  if (window.scrollY > 40) {
+    header.classList.add("HeaderScrolled");
+  }
+  else {
+    header.classList.remove("HeaderScrolled");
+  }
+
+});
+
+const reveals = document.querySelectorAll(".Reveal");
+
+window.addEventListener("scroll", () => {
+
+    reveals.forEach(element => {
+
+        const windowHeight = window.innerHeight;
+        const revealTop =
+            element.getBoundingClientRect().top;
+
+        if (revealTop < windowHeight - 100) {
+            element.classList.add("ActiveReveal");
+        }
+
+    });
+
+});
+
+document.addEventListener("mousemove", (event) => {
+  const x = (event.clientX / window.innerWidth - 0.5) * 15;
+  const y = (event.clientY / window.innerHeight - 0.5) * 15;
+
+  document.documentElement.style.setProperty("--starsx", `${x}px`);
+  document.documentElement.style.setProperty("--starsy", `${y}px`);
+});
+
+const sections = document.querySelectorAll("main section");
+const menuLinks = document.querySelectorAll(".MenuNavLink");
+
+function updateActiveMenuLink() {
+    let currentSection = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+
+        if (window.scrollY >= sectionTop - 180) {
+            currentSection = section.getAttribute("id");
+        }
+    });
+
+    const isBottomPage = window.innerHeight + window.scrollY >= document.body.offsetHeight - 5;
+
+    if (isBottomPage) {
+        currentSection = "contact";
+    }
+
+    menuLinks.forEach(link => {
+        link.classList.remove("ActivePage");
+
+        if (link.getAttribute("href") === `#${currentSection}`) {
+            link.classList.add("ActivePage");
+        }
+    });
+}
+
+window.addEventListener("scroll", updateActiveMenuLink);
+window.addEventListener("DOMContentLoaded", updateActiveMenuLink);
